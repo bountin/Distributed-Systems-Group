4bd59d2dfc4c3be39a434b687a0413ef54dfbda4
Martin Prebio <martin.prebio@gmail.com>
2014-01-05 17:14:52 +0000

    Adding HMAC Utility

---
+++ src/main/java/util/HMAC.java
@@ -0,0 +1,30 @@
+package util;
+
+import org.bouncycastle.util.encoders.Base64;
+import org.bouncycastle.util.encoders.Hex;
+
+import javax.crypto.Mac;
+import javax.crypto.spec.SecretKeySpec;
+import java.io.FileInputStream;
+import java.security.Key;
+
+public class HMAC {
+	static public String getHMAC(String message, String keyPath) throws HMACException {
+		try {
+			byte[] keyBytes = new byte[1024];
+			FileInputStream fis = new FileInputStream(keyPath);
+			fis.read(keyBytes);
+			fis.close();
+
+			byte[] input = Hex.decode(keyBytes);
+			Key secretKey = new SecretKeySpec(input, "HmacSHA256");
+			Mac hMac = Mac.getInstance("HmacSHA256");
+			hMac.init(secretKey);
+			hMac.update(message.getBytes());
+			byte[] hash = hMac.doFinal();
+			return new String(Base64.encode(hash));
+		} catch (Throwable e) {
+			throw new HMACException(e);
+		}
+	}
+}
---
+++ src/main/java/util/HMACException.java
@@ -0,0 +1,7 @@
+package util;
+
+public class HMACException extends Exception {
+	public HMACException(Throwable e) {
+		super(e);
+	}
+}
